c32c24dfad4a80375ea6c4e116560dfba7ba2e7d
TomKauffeld <tom.kauffeld@gmail.com>
2018-12-05 11:45:12 +0100

    adding an exemple usage

---
+++ exemple.js
@@ -0,0 +1,34 @@
+
+
+
+
+// Defining the API
+// the url is https://localhost/api/{version}/
+// we are using authentication
+// we are using version v1 (we can also say "latest")
+let api = new cov.api.node.Api( "https://localhost/api/{version}/", true, "v1");
+
+// adding the status route (at /api/{version}/status via a GET request)
+api.addRoute( "GET", "/status/", "status");
+
+// adding a node called user with the fields id of type int, first_name and last_name of type string
+api.addNode( "user", {
+    id:"int",
+    first_name:"string",
+    last_name:"string"
+});
+
+// logging in with the username "user" and the password "password"
+api.login( "user", "password");
+
+//once logged in execute this (it will wait until someone logs in or if the token is already valid)
+api.afterValid(true)
+    .then(response => api.callRoute( "GET", "status")) // we are getting the status
+    .then(console.log) //lets console log the response
+    .catch(console.error); //we have an error, lets log it
+
+// this will only check the token, and refresh it if needed, but if nobody is logged in, it will fail
+api.afterValid()
+    .then(r=>api.get('user', 1, "first_name,last_name")) // let's get the first_name and last_name of the user with the id 1
+    .then(console.log) // let's log the user in the console
+    .then(console.error); //we have an error, lets log it
